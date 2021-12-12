8c2786acd6536807ee354ac0db88c61da502ca3f
Ramya  Ramamurthy <ramyaramamurthy@ramyas-air.lan>
2021-12-11 19:19:36 -0800

    Adding Deliverable 1 Challenge files

---
+++ Analysis_Projects_Folder/Pewlett-Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,27 @@
+-- Retirement Titles
+SELECT  e.emp_no,
+    e.first_name,
+e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;     
+
+-- Unique Titles
+SELECT  DISTINCT ON (emp_no) emp_no,
+    first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;   
+
+--Retiring Titles
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
